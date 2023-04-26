d60ae1358091b2b027122329da1c89093e96f1e8
SCARRAIDER <rsahanediriweera@gmail.com>
2023-04-26 17:54:00 +0530

    completed product new page ui designs

--- ecommerce-admin/pages/products/new.js
+++ ecommerce-admin/pages/products/new.js
@@ -1,9 +1,36 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct(){
+
+    const [title,setTitle] = useState();
+    const [description,setDescription] = useState();
+    const [price,setPrice] = useState();
+
     return(
         <Layout>
-            New Product
+            <div className="flex flex-col">
+                <h1 className="text-blue-900 mb-2 text-xl">
+                    New Product
+                </h1>
+                <label placeholder="Product Name">
+                    Product Name
+                </label>
+                <input type="text" placeholder="Product Name"/>
+                <label placeholder="Description">
+                    Description
+                </label>
+                <textarea placeholder="Description">
+                </textarea>
+                <label placeholder="Product Name">
+                    Price
+                </label>
+                <input type="number" placeholder="Price"/>
+                <button className="btn-primary">
+                    Save
+                </button>
+            </div>
+
         </Layout>
     );
 }
