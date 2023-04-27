1a8347883210ab048f85ddd13516a2de22431188
SCARRAIDER <rsahanediriweera@gmail.com>
2023-04-27 14:38:50 +0530

    completed adding products

---
+++ ecommerce-admin/pages/api/products.js
@@ -0,0 +1,17 @@
+import clientPromise from "@/lib/mongodb";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+
+const handle = async(req,res)=>{
+    const {method} = req;
+    await mongooseConnect();
+    if(method == 'POST'){
+        const {title,description,price} = req.body;
+        const productDoc = await Product.create({
+            title,description,price
+        });
+        res.json(productDoc);
+    }
+};
+
+export default handle;
--- ecommerce-admin/pages/products/new.js
+++ ecommerce-admin/pages/products/new.js
@@ -1,4 +1,6 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function NewProduct(){
@@ -6,30 +8,43 @@ export default function NewProduct(){
     const [title,setTitle] = useState();
     const [description,setDescription] = useState();
     const [price,setPrice] = useState();
+    const [goToProduct,setGoToProduct] = useState(false);
+    const router = useRouter();
+
+    const createProduct = async (ev) => {
+        ev.preventDefault();
+        const data = { title, description, price};
+        await axios.post('/api/products',data);
+        setGoToProduct(true);
+    }
+
+    if(goToProduct){
+        router.push('/products');
+    }
 
     return(
         <Layout>
-            <div className="flex flex-col">
+            <form onSubmit={createProduct} className="flex flex-col">
                 <h1 className="text-blue-900 mb-2 text-xl">
                     New Product
                 </h1>
                 <label placeholder="Product Name">
                     Product Name
                 </label>
-                <input type="text" placeholder="Product Name"/>
+                <input type="text" placeholder="Product Name" value={title} onChange={e=>setTitle(e.target.value)}/>
                 <label placeholder="Description">
                     Description
                 </label>
-                <textarea placeholder="Description">
+                <textarea placeholder="Description" value={description} onChange={e=>setDescription(e.target.value)}>
                 </textarea>
                 <label placeholder="Product Name">
                     Price
                 </label>
-                <input type="number" placeholder="Price"/>
+                <input type="number" placeholder="Price" value={price} onChange={e=>setPrice(e.target.value)}/>
                 <button className="btn-primary">
                     Save
                 </button>
-            </div>
+            </form>
 
         </Layout>
     );
